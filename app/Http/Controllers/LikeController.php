ef7b2b7846d6219f0de9154cca3f8cc3e2c55f51
yuri <yuri.th03+work@gmail.com>
2023-12-06 15:27:40 +0900

    削除機能実装完了

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -8,28 +8,33 @@ use App\Models\Tweet;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class LikeController extends Controller
-{
+class LikeController extends Controller {
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
-    {
+    public function index(Request $request) {
         try {
-            // すべてのいいねの数を取得
-            $likes = Like::all();
+            // すべてのいいねの情報を取得
+            $likes = Like::with('user')->get();
 
-            // ここで必要なデータを整形して返す（例：ツイートごとのいいね数）
-            $likeCounts = $likes->groupBy('tweet_id')->map->count();
+            // ツイートごとのいいね数といいねをしたユーザー情報を格納する連想配列
+            $likeCounts = $likes->groupBy('tweet_id')->map(function ($group) {
+                return [
+                    'like_count' => $group->count(),
+                    'users' => $group->map(function ($like) {
+                        return $like->user->name;
+                    })->unique()->toArray(),
+                ];
+            });
 
             // デバッグ情報をログに書き込む
-            \Log::info('LikeController@index Debug: ' . json_encode($likeCounts));
+            \Log::info('LikeController@index Debug: '.json_encode($likeCounts));
 
             return response()->json(['data' => $likeCounts], 200);
         } catch (\Exception $e) {
-            \Log::error('LikeController@index Error: ' . $e->getMessage());
+            \Log::error('LikeController@index Error: '.$e->getMessage());
             return response()->json(['error' => 'Internal Server Error'], 500);
         }
     }
@@ -40,8 +45,7 @@ class LikeController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         try {
             $validator = Validator::make($request->all(), [
                 'tweet_id' => 'required',
@@ -49,24 +53,26 @@ class LikeController extends Controller
                 'id_token' => 'required|string',
             ]);
 
-            if ($validator->fails()) {
-                \Log::error('Validation Error: ' . json_encode($validator->errors()->toArray()));
+            if($validator->fails()) {
+                \Log::error('Validation Error: '.json_encode($validator->errors()->toArray()));
 
                 return response()->json(['error' => $validator->errors()], 422);
             }
 
             // Laravelデータベース内でFirebase UIDを使ってユーザーを取得
             $laravelUser = User::where('firebase_uid', $request->uid)->first();
-            \Log::info('Debug: ' . json_encode($laravelUser));
+            \Log::info('Frontend User Display Name: '.$request->header('X-User-Display-Name'));
 
-            if (!$laravelUser) {
+            \Log::info('Debug: '.json_encode($laravelUser));
+
+            if(!$laravelUser) {
                 return response()->json(['error' => 'User not found'], 404);
             }
 
             $user_id = $laravelUser->id;
             $tweet = Tweet::where('id', $request->tweet_id)->first();
 
-            if ($tweet) {
+            if($tweet) {
                 Like::updateOrCreate(
                     ['user_id' => $user_id, 'tweet_id' => $request->tweet_id],
                     ['user_id' => $user_id, 'tweet_id' => $request->tweet_id]
@@ -75,7 +81,7 @@ class LikeController extends Controller
             }
 
         } catch (\Exception $e) {
-            \Log::error('TweetController@store Error: ' . $e->getMessage());
+            \Log::error('TweetController@store Error: '.$e->getMessage());
             \Log::error($e);
             return response()->json(['error' => 'Internal Server Error'], 500);
         }
@@ -87,8 +93,7 @@ class LikeController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+    public function show($id) {
         //
     }
 
@@ -99,8 +104,7 @@ class LikeController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id) {
         //
     }
 
@@ -110,24 +114,19 @@ class LikeController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Request $request, $id)
-    {
-        \Log::info('Before Log Statement');
-        \Log::info('Tweet ID: ' . $id);
-        \Log::error('Tweet not found for ID: ' . $id);
-        \Log::info('After Log Statement');
+    public function destroy(Request $request, $id) {
 
         $userUid = $request->header('X-User-UID');
         $user = User::where('firebase_uid', $userUid)->first();
 
-        if ($user) {
-            $like = Like::where('tweet_id', $id)->first();
+        if($user) {
+            $like = Like::where('tweet_id', $id)->where('user_id', $user->id)->first();
 
-            if (!$like) {
+            if(!$like) {
                 return response()->json(['error' => 'Tweet not found'], 404);
             }
 
-            if ($like->user_id === $user->id) {
+            if($like->user_id === $user->id) {
                 $like->delete();
                 return response()->json(['message' => 'Tweet deleted successfully']);
             } else {
